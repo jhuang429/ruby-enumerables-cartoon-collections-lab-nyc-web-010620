d087ce32f3fe1f129fb300196efb0139a4e7a543
Jerry Huang <jhuang9240@gmail.com>
2019-10-30 15:48:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,8 +15,9 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
+  result = nil
   cheese_types = ["cheddar", "gouda", "camembert"]
   if array.include?(cheese_types)
-    array.index
+    result array.find
   end
 end
